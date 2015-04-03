e01415a6cf595f0d933115f1e9cdf5b7e9c22bbf
Jason Yao <jy1299@nyu.edu>
2015-04-03 12:52:20 -0400

    The path ahead is dangerous, added a safety pig for safe measure before running through valgrind

---
+++ safetypig.c
@@ -0,0 +1,18 @@
+/* WARNING: The following code may make you cry. A safety pig has been included for your benefit.
+*
+*
+*	 _._ _..._ .-',     _.._(`))
+*	'-. `     '  /-._.-'    ',/
+*	   )         \            '.
+*	  / _    _    |             \
+*	 |  a    a    /              |
+*	 \   .-.                     ;
+*	  '-('' ).-'       ,'       ;
+*	     '-;           |      .'
+*	        \           \    /
+*	        | 7  .__  _.-\   \
+*	        | |  |  ``/  /`  /
+*	       /,_|  |   /,_/   /
+*	          /,_/      '`-'
+*
+*/
